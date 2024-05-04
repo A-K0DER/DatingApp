918d0e59d277fdc3e4d34ef4df0771da832fde53
Ayush Kumar <ayushspidey@gmail.com>
2024-05-04 17:54:39 +0530

    home page and observables

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -9,24 +11,21 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model : any = {};
-  loggedIn = false;
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    
   }
 
   login(){
     this.accountService.login(this.model).subscribe({
       next: res => {
         console.log(res);
-        this.loggedIn = true;
       },
       error : err => console.log(err)
     })
   }
 
   logout(){
-    this.loggedIn = false;
+    localStorage.removeItem('user');
   }
 }

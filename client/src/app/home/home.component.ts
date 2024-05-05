e69ea7073ec60739532a6acef93d9dc0f233e8d4
Ayush Kumar <ayushspidey@gmail.com>
2024-05-05 11:37:29 +0530

    component communication

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-
+  users : any;
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
     
+  }
+ 
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users/GetUsers').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    });
   }
 
   registerToggle(){
     this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean){
+    this.registerMode = event;
   }
 }
